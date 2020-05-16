58e73b88267c79c244272b3584981c4a5eb90c4a
kawtarsidk <kawtar.sidk@gmail.com>
2020-05-16 23:11:57 +0000

    classes employe-rh-salarie

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Employe(models.Model):
+
+    matricule = models.CharField(max_length=20)
+    nom = models.CharField(max_length=30)
+    prenom = models.CharField(max_length=30)
+    cin = models.CharField(max_length=10)
+    adresse = models.CharField(max_length=150)
+    telephone = models.CharField(max_length=10)
+    dateNaissance = models.DateField()
+    departement = models.CharField(max_length=30)
+    emploiOccupe = models.CharField(max_length=30)
+    Anciennete = models.IntegerField(max_length=30)
+    salaireBase = models.IntegerField(max_length=10)
+
+    def __init__(self):
+        self.__init__()
+
+
+class RH(Employe):
+    def __init__(self):
+        super().__init__()
+
+class Salarie(Employe):
+    def __init__(self):
+        super().__init__()
+
