e9e2de465c67acc26325f34944b03eb48ee5a048
kaoutar karchaou <kawtarlulu.kl@gmail.com>
2020-05-17 02:34:17 +0100

    created the logging template

--- gestion/views.py
+++ gestion/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return  HttpResponse("welcome test")
+    # return  HttpResponse("welcome test")
+    return render(request, "login.html")
