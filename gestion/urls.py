fe8ca424ec185891d3c1a3fe6c4c85e238db398a
kaoutar karchaou <kawtarlulu.kl@gmail.com>
2020-05-20 17:27:47 +0100

    finalisation de la partie authentification et redirection vers la page d'accueil

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
+from django.urls import path
 
+from gestion import views
 from gestion.views import *
 
 urlpatterns = [
-    url('welcome', view=index),
+    path('welcome', view=index),
+    path('accueil', view=accueil, name='accueil')
+
+   # url('form', view=ajoutEmploye),
 
 
 ]
--- gestion/views.py
+++ gestion/views.py
@@ -1,30 +1,34 @@
-
+from gestion.forms import EmpForm
 from gestion.models import *
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import  UserCreationForm
+from django.contrib.auth import authenticate,login, logout
+from .urls import *
 
-
-def checkCompte(login, password):
-
-    compte = Compte.objects.all()
-    for c in compte:
-        if c.login == login and c.password == password:
-            return 'true'
-        else:
-            return 'false'
-
+def accueil(request):
+    return render(request, "accueil.html")
 
 def index(request):
-
     if request.method == 'POST':
-        login = request.POST.get('login')
+        uname = request.POST.get('login')
         password = request.POST.get('password')
-        if checkCompte(login, password) == "true":
-           # return HttpResponse('c est un compte')
-            return render(request, "accueil.html")
+        user = authenticate(request, username=uname, password=password)
+        if user is not None:
+            login(request,user)
+            return  redirect('gestion:accueil')
+
         else:
-            return HttpResponse('erreur')
+            messages.error(request,"mot de pass ou login incorrects")
 
     return render(request, "login.html")
 
 
+def ajoutEmploye(request):
+    form = EmpForm()
+    if request.method == 'POST':
+        form = EmpForm(request.POST)
+        form.save()
+        messages.success(request, 'bien ajoute')
+    return render(request, "accueil.html", {'form': form})
