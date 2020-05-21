089aa4fd0efe48b1a579e22db461fe5aa865de3a
kaoutar karchaou <kawtarlulu.kl@gmail.com>
2020-05-21 21:43:52 +0100

    resolution du probleme de comte creation du formulaire d'ajout de salaries changement du design ajout du framework bulma affichage des salaries enregoistres affichage des detail d'un salarie dans la liste affichee

--- gestion/admin.py
+++ gestion/admin.py
@@ -3,7 +3,6 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Employe)
-admin.site.register(Compte)
 admin.site.register(RH)
 admin.site.register(Salarie)
 admin.site.register(BulletinPaie)
